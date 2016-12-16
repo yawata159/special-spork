1ebad67cd6e9997aa1a04ecd545e8e303fc82e38
AWinnerIsYou <seijiyawata@gmail.com>
2016-12-16 11:45:10 -0500

    asdfasd

---
+++ pipe_networking.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include "pipe_networking.h"
+
+int client_handshake(int *to_server) {
+}
+
+int server_handshake(int *from_client) {
+
+}
