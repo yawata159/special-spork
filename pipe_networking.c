d10aad1de017a0a072bbfe73f1ec43db5bc8bb5b
achencoms <achencoms@gmail.com>
2016-12-18 18:15:42 -0500

    yes

--- pipe_networking.c
+++ pipe_networking.c
@@ -9,14 +9,26 @@
 
 int client_handshake(int *to_server) {
     int pid = getpid();
-    mkfifo( //do some stuff 
+    mkfifo("luigi", 0644); //do some stuff 
+    int pkw = open("mario", O_WRONLY);
+    write(pkw,"luigi", 5);
+    int pp = open("luigi", O_RDWR);
+    char ack[MESSAGE_BUFFER_SIZE];
+    read(pp, ack, sizeof(ack));
+    int i = fork();
+    if(!i) execlp("rm","rm","luigi",NULL);
+    write(pp,"CONNECTED",9);
 }
 
 int server_handshake(int *from_client) {
    char buffer[MESSAGE_BUFFER_SIZE];
    mkfifo("mario", 0644);
-   printf("[SERVER] Created WKP");
+   printf("[SERVER] Created WKP\n");
    int wkp = open("mario", O_RDONLY);
    read(wkp, buffer, sizeof(buffer));
+   int j = fork();
+   if(!j) execlp("rm","rm","mario",NULL);
+   int pp2 = open(buffer, O_WRONLY);
+   write(pp2,"connected", 9);
 
 }
